3f5f69f5fe8e2ac2507054db00c1ba29c25750db
AhmedGhnam <ahmedghnam116@gmail.com>
2019-12-09 06:35:00 +0200

    lesson 14

--- stage/js/main.js
+++ stage/js/main.js
@@ -4,5 +4,12 @@ $(function () {
     $(".content-area, .sidebar").toggleClass("no-sidebar");
   });
 
+  // toggle submenu 
+  $(".toggle-submenu").on("click", function() {
+    $(this).find(".fa-angle-right").toggleClass("down");
+    $(this).next('.child-links').slideToggle();
+    
+  });
+
 
 });
